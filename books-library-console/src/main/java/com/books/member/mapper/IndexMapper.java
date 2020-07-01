4581413f842cca1d047488e04034732a566a3fc5
zhaoyongbing <714835131@qq.com>
2020-07-01 21:52:18 +0800

    no message

--- books-library-console/src/main/java/com/books/member/mapper/IndexMapper.java
+++
@@ -1,9 +0,0 @@
-package com.books.member.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface IndexMapper extends BaseMapper<Member> {
-
-    Member select(int i);
-}
--- books-library-console/src/main/java/com/books/member/service/IndexService.java
+++
@@ -1,8 +0,0 @@
-package com.books.member.service;
-
-public interface IndexService {
-
-	Member demo();
-
-	Member demo2();
-}
